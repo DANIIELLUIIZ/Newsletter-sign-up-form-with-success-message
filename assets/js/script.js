04c5e7b8cdca8f505ae7c004ae8a11fdf25af458
Daniel Freitas <danielluiz358@gmail.com>
2023-10-27 21:34:42 -0300

    feat: tag picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,1 @@
-const IMAGE = document.querySelector('#image')
 
-document.body.onresize = function () {
-  if (document.body.clientWidth < 780) {
-    IMAGE.setAttribute('src', './assets/images/illustration-sign-up-mobile.svg')
-  } else {
-    IMAGE.setAttribute(
-      'src',
-      './assets/images/illustration-sign-up-desktop.svg'
-    )
-  }
-}
