90af3e23bb7b1bafe746853139f1aba7e902fa46
Daniel Freitas <danielluiz358@gmail.com>
2023-10-28 21:05:18 -0300

    resolvido bug do alert não aparecer e retorno a cor original do input depois de um e-mail invalido após 1s

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
 const EMAIL = document.querySelector('#email-field')
 const SUBMIT = document.querySelector('#submit-email')
+const alertMessage = document.querySelector('.alert-message')
 const returnBtn = document.querySelector('#return-btn')
-
-
-
-
-
 
 function submitEmail() {
   let emailValue = EMAIL.value
@@ -15,23 +11,24 @@ function submitEmail() {
     stateSucess()
     returnMessageSucess(emailValue)
   } else {
-    alertMessage.classList.toggle('hide')
+    alertMessage.classList.remove('hide')
     EMAIL.classList.add('alert')
+
+    setTimeout(function () {
+      EMAIL.classList.remove('alert')
+    }, 1000)
   }
-  EMAIL.value = ''
 }
 function stateSucess() {
   const main = document.querySelector('.main')
   const attribution = document.querySelector('.attribution')
-  const alertMessage = document.querySelector('.alert-message')
   const submitStates = document.querySelector('.submit-states')
 
   main.classList.toggle('hide')
   attribution.classList.toggle('hide')
-  EMAIL.classList.toggle('alert')
-  alertMessage.classList.toggle('hide')
   submitStates.classList.toggle('hide')
 }
+
 function returnMessageSucess(email) {
   const submitMessageSucess = document.querySelector('.state-message-sucess')
 
@@ -54,5 +51,9 @@ SUBMIT.addEventListener('click', e => {
 })
 
 returnBtn.addEventListener('click', () => {
+  alertMessage.classList.add('hide')
+  EMAIL.classList.remove('alert')
+
   stateSucess()
+  EMAIL.value = ''
 })
